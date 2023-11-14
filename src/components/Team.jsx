334cc33c0d8173c625548746c8c44c2e9d2b615d
Syed Sharukhali Farookali Naziyabegum <99337487+AI-ApeX-DeV@users.noreply.github.com>
2023-11-14 11:01:34 +0000

    .

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -75,7 +75,7 @@ const Team = () => {
       <div className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}>
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>Our Team</p>
-          <h5 className={styles.sectionHeadText}>{teamTitle}</h5>
+          <p className={styles.sectionHeadText}>{teamTitle}</p>
           <button onClick={switchTeam}>Switch</button>
         
             <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7 justify-center`}>
